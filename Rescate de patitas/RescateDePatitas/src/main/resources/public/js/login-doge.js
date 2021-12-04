8226b7282da64ff2aff94fd63281b603a492f7b7
Luciano Capomolla <lucho.capomolla@hotmail.com>
2021-12-04 15:58:18 -0300

    Fix del Login

--- Rescate de patitas/RescateDePatitas/src/main/resources/public/js/login-doge.js
+++ Rescate de patitas/RescateDePatitas/src/main/resources/public/js/login-doge.js
@@ -4,16 +4,16 @@ let images = ['https://res.cloudinary.com/rescatedepatitas08/image/upload/v16385
     'https://res.cloudinary.com/rescatedepatitas08/image/upload/v1638577753/img/login3_huxstc.png'];
 
 let index = 0;
-const imgElement = document.getElementById('doge');
-imgElement.src = images[index];
+let imgElement = document.getElementById('doge');
+imgElement.src = "https://res.cloudinary.com/rescatedepatitas08/image/upload/v1638577754/img/login1_pkgal6.png";
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function change() {
-    imgElement.src = images[1];
-    sleep(250).then(() =>{imgElement.src = images[0]})
+    imgElement.src = "https://res.cloudinary.com/rescatedepatitas08/image/upload/v1638577761/img/login2_k7ulyb.png";
+    sleep(250).then(() =>{imgElement.src = "https://res.cloudinary.com/rescatedepatitas08/image/upload/v1638577754/img/login1_pkgal6.png"})
 }
 
 let interval = null
@@ -24,10 +24,10 @@ window.onload = function () {
 
 function esconder() {
     window.clearTimeout(interval);
-    imgElement.src = images[2];
+    imgElement.src = "https://res.cloudinary.com/rescatedepatitas08/image/upload/v1638577753/img/login3_huxstc.png";
 }
 
 function continuar() {
-    imgElement.src = images[0];
+    imgElement.src = "https://res.cloudinary.com/rescatedepatitas08/image/upload/v1638577754/img/login1_pkgal6.png";
     interval = setInterval(change, 3000);
 }
